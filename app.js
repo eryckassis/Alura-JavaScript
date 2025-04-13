97be923ecca2ef39f8e89f7b65a09788c205ae2e
Eryck Assis <eng.assis.dev@gmail.com>
2025-04-12 23:42:40 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,17 +1,39 @@
 alert("Boas vindas ao jogo do numero secreto!");
 
-let numeroSecreto = 4; // numero secreto
+let numeroMaximo = 20; // numero maximo
+
+let numeroSecreto; // numero secreto
+
+numeroSecreto = parseint(Math.random() * numeroMaximo + 1);
 
 onsole.log("Voce acertou!");
 
-let chute = prompt("Digite um numero entre 1 e 30");
+let chute;
+let tentativas = 1; // contador de tentativas
 
-// se o chute for igual ao numero secreto
+// enquanto n for igual ao numero secreto
+while (chute != numeroSecreto) {
+  break;
 
-if (numeroSecreto == chute) {
-  alert("Isso aí voce descobriu o numero secreto ${numeroSecreto}");
-} else {
-  if (numeroSecreto > chute) {
-    alert(`onumero secreto e maior que ${chute}`);
+  chute = prompt(`Digite um numero entre 1 e ${numeroMaximo}`); // chute do usuario
+  if (chute == numeroSecreto) {
+  } else {
+    if (numeroSecreto > chute) {
+      alert(`o numero secreto e maior que ${chute}`);
+    } else {
+      alert(`o numero secreto e menor que ${chute}`);
+    }
+    tentativas++;
   }
+}
+let palavraTentativas = tentativas > 1 ? "tentativas" : "tentativa"; // operadopr ternario
+if (tentativas > 1) {
+  alert(
+    `Isso aí voce descobriu o numero secreto ${numeroSecreto} com {tentativas} tentativas!`
+  );
+  // se o chute for igual ao numero secreto
+} else {
+  alert(
+    `Isso aí voce descobriu o numero secreto ${numeroSecreto} com {tentativas} tentativa!`
+  );
 }
